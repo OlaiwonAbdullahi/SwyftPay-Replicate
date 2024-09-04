3e4ed16d82895cc2c6e7ae9400a40383c22d2eb3
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-04 21:23:03 +0100

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import Facebook from "../assets/Facebook.png";
 
 export default function Footer() {
   return (
-    <div className="bg-white mt-14 mx-12 md:mx-32 md:w-2/3">
+    <div className="bg-white mt-14 mx-12 md:mx-32 md:w-2/3 md:hidden">
       <img src={Logo} className="h-16 w-24 mb-6"></img>
       <div className=" flex gap-3">
         <div className="basis-1/3 mt-5">
