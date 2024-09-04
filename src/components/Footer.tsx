4df4321379dea9728f0878344b7eb266d28d76e1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-04 21:00:51 +0100

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 import Logo from "../assets/logo.png";
+import LinkedIn from "../assets/linkedin.png";
+import X from "../assets/X.png";
+import Instagram from "../assets/Instagram.png";
+import Facebook from "../assets/Facebook.png";
+
 export default function Footer() {
   return (
     <div className="bg-white mt-14 mx-12 md:mx-32 md:w-2/3">
-      <img src={Logo} className="h-16 w-24 mb-4"></img>
+      <img src={Logo} className="h-16 w-24 mb-6"></img>
       <div className=" flex gap-3">
         <div className="basis-1/3 mt-5">
           <h2 className="mb-6 text-base text-gray-900 font-semibold font-inter">
@@ -15,7 +20,7 @@ export default function Footer() {
           <h2 className="mb-6 text-base text-gray-900 font-semibold font-inter">
             Company
           </h2>
-          <span className="text-sm text-gray-500 font-inter">Careers</span>
+          <span className="text-sm text-gray-500 font-inter mb-5">Careers</span>
           <span className="text-sm text-gray-500 font-inter">Blog</span>
         </div>
 
@@ -23,11 +28,17 @@ export default function Footer() {
           <h2 className="mb-6 text-base text-gray-900 font-semibold font-inter">
             Legal
           </h2>
-          <span className="text-sm text-gray-500 font-inter">
+          <span className="text-sm text-gray-500 font-inter mb-5">
             Privacy Policy
           </span>
           <span className="text-sm text-gray-500 font-inter">Terms of use</span>
         </div>
+      </div>
+      <div className="socials flex">
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
       </div>
     </div>
   );
