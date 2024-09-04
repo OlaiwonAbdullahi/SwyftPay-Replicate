52c67c4dae50ab3ac107ed3ecc8451852023cab4
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-04 09:14:26 +0100

    update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,17 +2,19 @@ import HeroImg from "../assets/hero.svg";
 export default function Hero() {
   return (
     <div className="flex flex-col md:flex-row w-auto h-auto">
-      <div className=" text-center basis-1/3 justify-between">
-        <div className="flex gap-2 bg-violet-200 rounded-xl h-8 w-52 pt-1">
+      <div className=" text-center basis-1/3 justify-center mt-4">
+        <div className="flex gap-2 bg-violet-200 rounded-2xl h-8 w-52 pt-1 mx-auto mb-5 mt-7 md:mt-36">
           <span className="bg-violet-500 text-white rounded-xl ml-2 h-6 text-sm w-10">
             NEW
           </span>
           <p>Available only in 🇳🇬.</p>
         </div>
-        <h2 className="text-4xl text-slate-900 font-extrabold">
+        <h2 className="text-4xl md:text-5xl text-slate-900 font-extrabold mt-3">
           Global Remittance,
         </h2>
-        <h2 className="text-4xl text-violet-700 font-extrabold">Made Easy.</h2>
+        <h2 className="text-4xl md:text-5xl md:pl-4 text-violet-700 font-extrabold mt-3">
+          Made Easy.
+        </h2>
       </div>
       <div className=" flex md:items-end items-center basis-2/3 justify-end">
         <img src={HeroImg}></img>
