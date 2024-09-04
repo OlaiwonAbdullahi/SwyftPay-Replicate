ac409917b7cb34867949c33467f0d6a066deef11
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-04 13:13:36 +0100

    update 404.tsx

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,7 +1,7 @@
 export default function Four() {
   return (
-    <h2 className="text-5xl text-violet-600 text-center">
-      Comrade i Neva Work Reach That Side😒😒
+    <h2 className="text-4xl text-violet-600 text-center mt-32">
+      Comrade CalmDown i Neva Work Reach That Side😒😒
     </h2>
   );
 }
